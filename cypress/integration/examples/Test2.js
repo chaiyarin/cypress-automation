c13acbe566bba4995776260d7eb0661eee1f74d0
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2020-05-31 15:43:11 +0700

    วิธีเขียน Test Case ของอะไรที่เป็น checkbox

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,10 @@
+describe('Basic UI', () => {
+    it('Basic UI Test Case', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice');
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check()
+        cy.get('inpit[type="checkbox"]').uncheck()
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    })
+})
