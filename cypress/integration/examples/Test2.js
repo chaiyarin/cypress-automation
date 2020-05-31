4e5dc795d7fbc3539a445182942e2d40eccf6c60
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2020-05-31 15:52:32 +0700

    เพิ่ม Test Case เกี่ยวกับ Dropdown ที่เป็น Static

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,10 +1,18 @@
 describe('Basic UI', () => {
     it('Basic UI Test Case', () => {
+
+        // CheckBox
         cy.visit('https://rahulshettyacademy.com/AutomationPractice');
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check()
-        cy.get('inpit[type="checkbox"]').uncheck()
+        cy.get('input[type="checkbox"]').uncheck()
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+
+        // Static Dropdown
+        cy.get('select').select('option2').should('have.value', 'option2')
+
+
     })
 })
