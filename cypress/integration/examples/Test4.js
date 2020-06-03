c2abb3be3d1f1ce884267c9944ff8828aa8d938e
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2020-06-03 14:59:26 +0700

    รู้จักกับ คำสั่ง cy.go('back') เสมือนกันกดปุ่ม back บน Chrome และ การ assert ว่าใน url นี้มี keyword อะไีรอยู่

--- cypress/integration/examples/Test4.js
+++ cypress/integration/examples/Test4.js
@@ -3,5 +3,7 @@ describe('Test Tab', () => {
     it('Test Case Tab', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice')
         cy.get('#opentab').invoke('removeAttr', 'target').click()
+        cy.url().should('include', 'index')
+        cy.go('back')
     })
 })
