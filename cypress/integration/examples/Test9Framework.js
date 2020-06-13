ce9d994eb01b9922beabe207ebadd18f30699cc1
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2020-06-13 14:30:41 +0700

    เพิ่ม Keyword สำหรับ ทำเป็น Function กลางใน Cypress Command

--- cypress/integration/examples/Test9Framework.js
+++ cypress/integration/examples/Test9Framework.js
@@ -3,13 +3,22 @@ describe('Test Cypress Framework', () => {
 
     before(() => {
         cy.fixture('example').then((data) => {
-            this.data = data
+            // this.data = data
         })
     })
 
     it('Test Case', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice')
-        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-        cy.get('select').select(this.data.gender)
+        // cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        // cy.get('select').select(this.data.gender)
+        cy.get(':nth-child(2) > .nav-link').click()
+        // cy.get('h4.card-title').each(($el, index, $list) => {
+        //     if($el.text().includes('Blackberry'))
+        //     {
+        //         cy.get('button.btn.btn-info').eq(index).click()
+        //     }
+        // })
+        cy.selectProduct('iphone X')
+
     })
 })
