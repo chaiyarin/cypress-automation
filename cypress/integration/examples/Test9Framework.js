229216ff2ca7472ae1d2ac7a62e572f49f6657f7
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2020-06-18 20:47:01 +0700

    เขียน Test Case จาก การวน Loop จาก ชุดข้อมูลใน Test Fixture

--- cypress/integration/examples/Test9Framework.js
+++ cypress/integration/examples/Test9Framework.js
@@ -1,16 +1,18 @@
 /// <reference types="Cypress" />
+var data
 describe('Test Cypress Framework', () => {
 
     before(() => {
-        cy.fixture('example').then((data) => {
-            // this.data = data
+        cy.fixture('example').then(function (example_data) {
+            cy.log(data)
+            data = example_data
         })
     })
 
     it('Test Case', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice')
-        // cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-        // cy.get('select').select(this.data.gender)
+        cy.get('input[name="name"]:nth-child(2)').type(data.name)
+        cy.get('select').select(data.gender)
         cy.get(':nth-child(2) > .nav-link').click()
         // cy.get('h4.card-title').each(($el, index, $list) => {
         //     if($el.text().includes('Blackberry'))
@@ -18,7 +20,9 @@ describe('Test Cypress Framework', () => {
         //         cy.get('button.btn.btn-info').eq(index).click()
         //     }
         // })
-        cy.selectProduct('iphone X')
+        data.productNames.forEach(element => {
+            cy.selectProduct(element)
+        });
 
     })
 })
